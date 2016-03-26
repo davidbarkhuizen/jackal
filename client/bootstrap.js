58aa24e983ce93f8a6fe62d8f777132ab7329c75
david <david.barkhuizen@gmail.com>
2016-03-26 22:34:59 +0200

    interface coming along

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,5 +1,29 @@
 var doc = {
-	title: "netnode"
+	title: "netnode",
+	bodyTemplateURL: "body.html",
 };
 
 document.title = doc.title;
+
+function loadBodyHTML(url, focusOnId) {
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url, true);
+
+	xhr.onreadystatechange= function() {
+	    
+	    if (this.readyState !== 4)
+	    	return;
+	    
+	    if (this.status !== 200)
+	    	return;
+
+	    document.body.innerHTML= this.responseText;
+
+	    document.getElementById(focusOnId).focus();
+	};
+
+	xhr.send();
+}
+
+loadBodyHTML(doc.bodyTemplateURL, 'stdin');
